4b6eb79b4e56c9a1dfbe7a97ddac75365c046011
Piter Capistrano <pitercapistrano@gmail.com>
2024-10-22 20:09:07 -0300

    Tela Principal Criada!

--- app/src/main/java/com/pitercapistrano/listadetarefacompose/view/ListaTarefas.kt
+++ app/src/main/java/com/pitercapistrano/listadetarefacompose/view/ListaTarefas.kt
@@ -1,9 +1,73 @@
 package com.pitercapistrano.listadetarefacompose.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.pitercapistrano.listadetarefacompose.R
+import com.pitercapistrano.listadetarefacompose.ui.theme.Blue
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ListaTarefas(navController: NavController){
+fun ListaTarefas(
+    navController: NavController
+){
 
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Lista de Tarefas", fontWeight = FontWeight.Bold, fontSize = 18.sp) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Blue,
+                    titleContentColor = Color.White
+                )
+            )
+        },
+        
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { navController.navigate("salvarTarefa") },
+
+                containerColor = Blue,
+                contentColor = Color.White,
+                shape = RoundedCornerShape(50.dp)
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_add),
+                    contentDescription = "Botão de adicionar tarefa"
+                )
+            }
+        },
+
+        containerColor = Color.Black
+    ) {
+        Column(
+            modifier = Modifier.padding(it)
+        ) {
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ListaTarefaPreview(){
+    ListaTarefas(navController = rememberNavController())
 }
